d470bfe27df0525b93f44caa3543e9351eada074
robert neriko <nerikorobert@gmail.com>
2023-08-28 09:59:23 +0300

    completed assignment

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,13 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+import Item from "./Item";
 
-function Item({ name, category }) {
+function ShoppingList({ items }) {
+  const [selectedCategory, setSelectedCategory]=useState("All");
+ 
+
+  // our filter function
+  function handleFilter(e){
+    setSelectedCategory(e.target.value);
+  }
+  const data = items.filter((item) => {
+    if (selectedCategory === "All") {
+      return true;
+    } else {
+      return item.category === selectedCategory;
+    }
+  });
+  const mydata = data.map((item) => {
+    return <Item key={item.id} name={item.name} category={item.category} id={item.id}/>;
+  });
+  
   return (
-    <li className="">
-      <span>{name}</span>
-      <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
-    </li>
+    <div className="ShoppingList">
+      <div className="Filter">
+        <select name="filter" onChange={handleFilter}>
+          <option value="All">Filter by category</option>
+          <option value="Produce">Produce</option>
+          <option value="Dairy">Dairy</option>
+          <option value="Dessert">Dessert</option>
+        </select>
+      </div>
+      <ul className="Items">
+          {mydata}
+      </ul>
+    </div>
   );
 }
 
-export default Item;
+export default ShoppingList;
